03c58774a6539c5a13894d1998f62b2a8edd94ae
John_Lamy <johnclamy@gmail.com>
2024-06-19 23:42:12 +0100

    setup the todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,18 @@
-const TodoList = () => {
-  return <section>todo list goes here...</section>;
+import { ReactElement } from "react";
+import TodoItem, { TodoProp } from "./TodoItem";
+
+type TodosProp = {
+  todos: TodoProp[];
+};
+
+const TodoList = ({ todos }: TodosProp): ReactElement => {
+  return (
+    <ul className="max-w-md mb-8 space-y-1 text-gray-500 list-inside">
+      {todos.map((todoItem) => (
+        <TodoItem key={todoItem.id} todoItem={todoItem} />
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
