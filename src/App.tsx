865cf50ff2fd1ea0bc9c6aabac8d4ef458d30ced
John_Lamy <johnclamy@gmail.com>
2024-06-19 23:45:10 +0100

    add the todo list component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ReactElement } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Searchbar from "./components/Searchbar";
 import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import { TodoProp } from "./components/TodoItem";
+import todos from "./assets/initialData.json";
 import "./App.css";
 
-function App() {
+export default function App(): ReactElement {
   return (
     <div>
       <Navbar />
@@ -12,9 +16,12 @@ function App() {
       <main className="container mx-auto max-w-96">
         <Searchbar />
         <AddTodo />
+        <hr className="h-px my-8 bg-gray-200 border-2" />
+        <h2 className="mb-3 text-lg capitalize font-semibold text-gray-900">
+          my todo list
+        </h2>
+        <TodoList todos={todos as TodoProp[]} />
       </main>
     </div>
   );
 }
-
-export default App;
