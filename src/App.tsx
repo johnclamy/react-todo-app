c7b53bd8d2aee0b2c370598756a9d04a667ca4b9
John_Lamy <johnclamy@gmail.com>
2024-06-20 14:29:33 +0100

    create a render func to display the todos when available

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Searchbar from "./components/Searchbar";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import { TodoProp } from "./components/TodoItem";
-import todos from "./assets/initialData.json";
+import defaultTodos from "./assets/initialData.json";
 import "./App.css";
 
 export default function App(): ReactElement {
+  const [todos, setTodos] = useState<TodoProp[]>(
+    defaultTodos || ([] as TodoProp[])
+  );
+
+  const handleAddTodo = (text: string) => {
+    const count = todos.length;
+    const id = count ? todos[count - 1].id + 1 : count;
+    const todo = {
+      id,
+      todo: text,
+      completed: false,
+      userId: Math.floor(Math.random() * 100) + 1,
+    };
+    setTodos([...todos, todo]);
+  };
+
+  const renderTodos = !todos.length ? (
+    <section className="flex justify-center my-8 mx-2 sm:mx-0 bg-yellow-50 border-spacing-2 border-yellow-100 rounded-md p-3 sm:p-5">
+      <p className="text-lg text-gray-500 md:text-xl text-center">
+        <strong>Oops!</strong> Currently <strong>no</strong> todos to be shown
+      </p>
+    </section>
+  ) : (
+    <TodoList todos={todos} />
+  );
+
   return (
     <div>
       <Navbar />
       <Header />
       <main className="container mx-auto max-w-96">
         <Searchbar />
-        <AddTodo />
+        <AddTodo onAddTodo={handleAddTodo} />
         <hr className="h-px my-8 bg-gray-200 border-2" />
-        <h2 className="mb-3 text-lg capitalize font-semibold text-gray-900">
+        <h2 className="mb-3 mx-2 sm:mx-0 text-lg capitalize font-semibold text-gray-900">
           my todo list
         </h2>
-        <TodoList todos={todos as TodoProp[]} />
+        {renderTodos}
       </main>
     </div>
   );
