a8a86404da2fcb7cef2432161bbece435ce499af
John_Lamy <johnclamy@gmail.com>
2024-06-21 23:25:00 +0100

    handle the onClick event in TodoItem to delete that todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import { MdEdit, /* MdEditOff, */ MdDelete } from "react-icons/md";
 import ListIcon from "./ListIcon";
 
 export type TodoProp = {
@@ -8,10 +9,32 @@ export type TodoProp = {
   userId?: number;
 };
 
-const TodoItem = ({ todoItem }: { todoItem: TodoProp }): ReactElement => (
-  <li className="flex items-center py-1">
-    <ListIcon colour="#2AAA8A" />
-    {todoItem.todo}
+export type TodoProps = {
+  todoItem: TodoProp;
+  onDeleteTodo: (todoId: number) => void;
+};
+
+const TodoItem = ({ todoItem, onDeleteTodo }: TodoProps): ReactElement => (
+  <li className="flex justify-between items-center bg-slate-100 mb-1 p-1 rounded-lg">
+    <div className="flex items-center py-1 my-2 mr-2">
+      <ListIcon colour="#2AAA8A" />
+      {todoItem.todo}
+    </div>
+    <div className="bg-white p-1 rounded-md grid gap-1">
+      <MdEdit
+        className="text-amber-500 border border-amber-500 rounded-full m-1 cursor-pointer"
+        title="Edit todo"
+      />
+      {/* <MdEditOff
+        className="text-amber-500 border border-amber-500 rounded-full m-1 cursor-pointer"
+        title="Editing todo"
+      /> */}
+      <MdDelete
+        className="text-red-500 border border-red-500 rounded-full m-1 cursor-pointer"
+        title="Delete todo"
+        onClick={() => onDeleteTodo(todoItem.id)}
+      />
+    </div>
   </li>
 );
 
